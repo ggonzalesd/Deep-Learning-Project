6980ebc899b4ce42c00248fd5570242d70bb73a4
Grober Gonzales <GonzalesDLCGrober@gmail.com>
2023-04-28 08:23:41 -0500

    Models

--- classification_models.py
+++ classification_models.py
@@ -19,16 +19,37 @@ def DownSample(input_, output_, kernel=3, padding=1, pooling=2, batch=True, acti
   seq.append(nn.MaxPool2d(pooling, stride=pooling))
   return seq
 
-class ClasificatorSimpleModel(nn.Module):
+class SimpleModelWithReLU(nn.Module):
   def __init__(self):
     super().__init__()
-    self.net = nn.Sequential(                             #   2  512 512
-      DownSample(2, 8, batch=False),                      #   8  256 256
-      DownSample(8, 16),                                  #   16 128 128
-      DownSample(16, 32),                                 #   32  64  64
-      DownSample(32, 64),                                 #   64  32  32
-      DownSample(64, 128, pooling=4, activation='LeakyReLU'),  #  128   8   8
-      DownSample(128, 512, pooling=8, activation='LeakyReLU'), #  512   1   1
+    self.net = nn.Sequential(                                  #   2  512 512
+      DownSample(2, 8, batch=False),                           #   8  256 256
+      DownSample(8, 16),                                       #   16 128 128
+      DownSample(16, 32),                                      #   32  64  64
+      DownSample(32, 64, pooling=4),                           #   64  16  16
+      DownSample(64, 128, pooling=4),                          #  128   4   4
+      DownSample(128, 512, pooling=4),                         #  512   1   1
+    )
+    self.classificator = nn.Sequential(
+      nn.Flatten(),
+      nn.Linear(512, 64),
+      nn.ReLU(),
+      nn.Linear(64, 3)
+    )
+
+  def forward(self, X):
+    return self.classificator(self.net(X))
+
+class SimpleModelWithSELU(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.net = nn.Sequential(                                  #   2  512 512
+      DownSample(2, 8, batch=False),                           #   8  256 256
+      DownSample(8, 16),                                       #   16 128 128
+      DownSample(16, 32),                                      #   32  64  64
+      DownSample(32, 64, pooling=4, activation='SELU'),        #   64  16  16
+      DownSample(64, 128, pooling=4, activation='SELU'),       #  128   4   4
+      DownSample(128, 512, pooling=4, activation='SELU'),      #  512   1   1
     )
     self.classificator = nn.Sequential(
       nn.Flatten(),
@@ -40,7 +61,31 @@ class ClasificatorSimpleModel(nn.Module):
   def forward(self, X):
     return self.classificator(self.net(X))
 
-class ClasificatorCustomModel(nn.Module):
+class DeeperModelWithLeakyReLU(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.net = nn.Sequential(                                  #   2  512 512
+      DownSample(2, 8, batch=False),                           #   8  256 256
+      DownSample(8, 16),                                       #   16 128 128
+      DownSample(16, 32),                                      #   32  64  64
+      DownSample(32, 64),                                      #   64  32  32
+      DownSample(64, 128, activation='LeakyReLU'),             #  128  16  16
+      DownSample(128, 512, pooling=4, activation='LeakyReLU'), #  512   4   4
+      DownSample(512, 512, pooling=4, activation='LeakyReLU'), #  512   1   1
+    )
+    self.classificator = nn.Sequential(
+      nn.Flatten(),
+      nn.Linear(512, 64),
+      nn.LeakyReLU(0.02),
+      nn.Linear(64, 16),
+      nn.LeakyReLU(0.02),
+      nn.Linear(16, 3)
+    )
+
+  def forward(self, X):
+    return self.classificator(self.net(X))
+
+class DeeperModelWithSELU(nn.Module):
   def __init__(self):
     super().__init__()
     self.net = nn.Sequential(                             #   2  512 512
@@ -48,15 +93,16 @@ class ClasificatorCustomModel(nn.Module):
       DownSample(8, 16),                                  #   16 128 128
       DownSample(16, 32),                                 #   32  64  64
       DownSample(32, 64),                                 #   64  32  32
-      DownSample(64, 128, pooling=4, activation='SELU'),  #  128   8   8
-      DownSample(128, 512, pooling=8, activation='SELU'), #  512   1   1
+      DownSample(64, 128, activation='SELU'),             #  128  16  16
+      DownSample(128, 512, pooling=4, activation='SELU'), #  512   4   4
+      DownSample(512, 512, pooling=4, activation='SELU'), #  512   1   1
     )
     self.classificator = nn.Sequential(
       nn.Flatten(),
       nn.Linear(512, 64),
       nn.ReLU(),
       nn.Linear(64, 16),
-      nn.ReLU(),
+      nn.SELU(),
       nn.Linear(16, 3)
     )
 
